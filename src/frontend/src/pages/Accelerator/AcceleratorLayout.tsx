453538f6ce2bac08aed7b8360438295958736d80
Bochaberi14 <bochaberiwinnie14@gmail.com>
2025-07-14 15:44:58 +0300

    updated startup details page

--- src/frontend/src/pages/Accelerator/AcceleratorLayout.tsx
+++ src/frontend/src/pages/Accelerator/AcceleratorLayout.tsx
@@ -1,23 +1,3 @@
-/*import React from 'react';
-import { Outlet } from 'react-router-dom';
-import AcceleratorSidebar from '../../components/layout/accelerator/AcceleratorSidebar';
-
-const AcceleratorLayout: React.FC = () => {
-    return (
-        <div className="flex h-screen bg-white">
-            <AcceleratorSidebar />
-            <div className="flex-1 flex flex-col overflow-hidden">
-                <main className="flex-1 overflow-x-hidden overflow-y-auto bg-white">
-                    <div className="container mx-auto px-6 py-8">
-                        <Outlet />
-                    </div>
-                </main>
-            </div>
-        </div>
-    );
-};
-
-export default AcceleratorLayout;*/
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import AcceleratorSidebar from '../../components/layout/accelerator/AcceleratorSidebar';
